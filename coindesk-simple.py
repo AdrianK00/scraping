8645691e6409e3fe4d8865e1336ad218d18dfe86
AK <kimadriankim@gmail.com>
2021-06-02 04:56:36 -0300

    added comments

--- coindesk-simple.py
+++ coindesk-simple.py
@@ -6,13 +6,20 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.text, "lxml")
 
-all_cryptos = soup.find("div", class_ = "pricing-container")
-cryptos = all_cryptos.find_all("div", class_ = "pricing-col")
+#Get all cryptos
+cryptos = soup.find_all("div", class_ = "pricing-col")
 
 for crypto in cryptos:
+
+    #Get all cryptos names
     crypto_name = crypto.find("div", class_ = "title").text.split()[0]
+
+    #Get all cryptos prices
     crypto_price_with_none = crypto.find("div", class_ = "price val-negative")
+
+    #Filter prices with None value
     if crypto_price_with_none != None:
         crypto_price = crypto.find("div", class_ = "price val-negative").text
+
     print(crypto_name)
     print(crypto_price)
